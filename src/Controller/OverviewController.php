124a32d9f21268bab202f6c92682e09da335aa63
Valentin <valentin.debonnefoy@gmail.com>
2023-04-06 19:43:49 +0200

    Valentin acceuil clear

---
+++ src/Controller/OverviewController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/overview', name: 'overview')]
+class OverviewController extends AbstractController
+{
+    #[Route('', name: '')]
+    public function indexAction(): Response
+    {
+        return $this->render('/Overview/Accueil.html.twig');
+    }
+}
